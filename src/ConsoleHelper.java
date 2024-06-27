c026f3b7473d6b1a4866e01d9751f0fea3ecee78
AVVoy <Aleksandr.V.Voynov@yandex.ru>
2024-06-27 23:14:00 +0300

    Added into ConsoleHelper methods ReadMessage and isEmpty into read Int

--- src/ConsoleHelper.java
+++ src/ConsoleHelper.java
@@ -11,11 +11,17 @@ public class ConsoleHelper {
         while (true){
             String s = scanner.nextLine();
             try {
+                if (s.isEmpty()) throw new NumberFormatException();
                 int a = Integer.parseInt(s);
                 return a;
             } catch (NumberFormatException e){
                 writeMessage("Это не число!!! Введите число!");
             }
         }
+    }
+
+    public static String readMessage() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 }
